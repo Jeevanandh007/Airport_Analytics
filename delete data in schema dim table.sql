908a841e0a97c0bf1c45e5621dcab67c59645261
Jeevanandh Vijayan <53204857+Jeevanandh007@users.noreply.github.com>
2024-07-16 04:20:31 +0530

    Add files via upload

---
+++ delete data in schema dim table.sql
@@ -0,0 +1,25 @@
+--delete from Dim_Airline
+--DBCC CHECKIDENT ('Dim_airline', RESEED, 0);
+
+--delete from Dim_GeoSummary
+--DBCC CHECKIDENT ('Dim_GeoSummary', RESEED, 0);
+
+--delete from Dim_BoardingArea
+--DBCC CHECKIDENT ('Dim_BoardingArea', RESEED, 0);
+
+--delete from Dim_ActivityType
+--DBCC CHECKIDENT ('Dim_ActivityType', RESEED, 0);
+
+--delete from Dim_GeoRegion
+--DBCC CHECKIDENT ('Dim_GeoRegion', RESEED, 0);
+
+--delete from Dim_Terminal
+--DBCC CHECKIDENT ('Dim_Terminal', RESEED, 0);
+
+--delete from Dim_Date
+--DBCC CHECKIDENT ('Dim_Date', RESEED, 0);
+
+
+
+
+Fact_PassengerCount
---
+++ select data.sql
@@ -0,0 +1,9 @@
+select * from Dim_ActivityType
+select * from Dim_GeoSummary
+select * from Dim_airline
+select * from Dim_GeoRegion
+select * from Dim_BoardingArea
+select * from Dim_Terminal
+select * from Dim_Date
+
+select * from Fact_PassengerCount
